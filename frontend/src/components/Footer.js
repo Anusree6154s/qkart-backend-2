8609ec17db91e78a5efdfc198e9bb4ed6360efa1
Simran Kumari <simran@criodo.com>
2023-12-05 12:46:09 +0530

    updated changes - 2023-12-05 12:46:08

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,13 @@
-import { Footer as AntdFooter } from "antd/lib/layout/layout";
 import React from "react";
-
 import "./Footer.css";
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <AntdFooter className="footer">
-        <img src="icon-white.png" alt="QKart"></img>
-      </AntdFooter>
+      <footer className="footer">
+        <img className="footer-img" src="icon-white.png" alt="QKart"></img>
+      </footer>
     );
   }
 }
+
