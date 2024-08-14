a9cb206599ff2c012fd6242cc3516d0625344b04
Anusree <anilkumaranusree113@gmail.com>
2024-08-14 12:37:36 +0530

    backend fix

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ app.use("/v1", routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
+    console.log(req.url)
     next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
 
