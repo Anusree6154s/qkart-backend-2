7a47236b84eccb412eb385adec81da57c2e1f7ab
Anusree <anilkumaranusree113@gmail.com>
2024-08-14 14:31:44 +0530

    backend fix

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ const { createUser } = require("./services/user.service");
 let server;
 
 // Enable Mongoose debugging to log all queries
-mongoose.set('debug',function(collectionName, methodName, ...methodArgs) {
+mongoose.set('debug', function (collectionName, methodName, ...methodArgs) {
   console.log('collectionName:', collectionName)
   console.log('methodName:', methodName)
   console.log('methodArgs:', methodArgs)
 });
 
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
-mongoose.connect(config.mongoose.url, config.mongoose.options, () => {
-  console.log("Successfully connected to MongoDB");
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  console.log('MongoDB connected');
   app.listen(config.port, () => {
     console.log(`Server connect to port ${config.port}`);
   });
-});
+  return mongoose.connection.db.collection('products').find({}).toArray();
+})
+  .then(products => {
+    console.log('Products:', products);
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
+;
