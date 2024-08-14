3028a475dcbbd817e9b5de222f34ea2205f0fd01
Anusree <anilkumaranusree113@gmail.com>
2024-08-14 14:19:57 +0530

    backend fix

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -6,20 +6,22 @@ const ApiError = require("../utils/ApiError");
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
     let { statusCode, message } = err;
+    let status = statusCode || httpStatus.INTERNAL_SERVER_ERROR
 
     res.locals.errorMessage = err.message;
 
     const response = {
-        code: statusCode,
-        message,
+        code: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+        message: message || 'Internal Server Error',
         ...(config.env === "development" && { stack: err.stack }),
     };
 
+    console.log('config.env:', config.env)
     if (config.env === "development") {
         console.error(err);
     }
 
-    res.status(statusCode).send(response);
+    res.status(status).send(response);
 };
 
 module.exports = {
